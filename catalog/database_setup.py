45f68e527a8b3afd3219e0c87eabbf5e22248d77
Shinsuke JJ Ikegame <shinsuke.ikegame@gmail.com>
2015-12-16 14:30:50 +0700

    Update database_setup.py

--- catalog/database_setup.py
+++ catalog/database_setup.py
@@ -13,7 +13,7 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    provider = Column(Integer, nullable=False)
+    provider = Column(String(100), nullable=False)
     name = Column(String(100), nullable=False)
     email = Column(String(100), nullable=False)
     picture = Column(String(200))
