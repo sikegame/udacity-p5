e351f12a75f67f6371e739e15e5512ff5c5907d1
Shinsuke JJ Ikegame <shinsuke.ikegame@gmail.com>
2015-12-16 15:38:41 +0700

    Update project.py

--- catalog/project.py
+++ catalog/project.py
@@ -564,7 +564,7 @@ def git_connect():
 
     # Prepare necessary information
     code = request.args.get('code')
-    result = json.loads(ABS_PATH + 'secrets.json', 'r').read()['github']
+    result = json.loads('/var/www/udacity-project-3/catalog/secrets.json', 'r').read()['github']
     client_id = result['client_id']
     client_secret = result['client_secret']
 
